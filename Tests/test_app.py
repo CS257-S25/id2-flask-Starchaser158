ac2e0a864bc6810aaabb3b525c022fa8b9dd0af8
Nicholas <nicholastheus922@gmail.com>
2025-04-25 12:55:56 -0500

    ID2 Take four

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -9,22 +9,24 @@ class TestHomepage(unittest.TestCase):
 
     def setUp(self):
         """Sets up the test client"""
+
         self.app = app.test_client()
 
-    """Tests the route for the homepage"""
-    def test_route(self):
-        self.app = app.test_client()
+    
+    def test_homepage(self):
+        """Test for route for the homepage"""
+
         response = self.app.get('/', follow_redirects=True)
-        self.assertEqual(b"Hello! Welcome to our website with the amazingly" \
-    "curated title: Analyzing Criminal Drug Abuse Treatment in Females" \
-    "\nAlso known as drug_abuse_treatment.py" \
-    "\n" \
-    "\n" \
-    "Here are the main usages of our program for your research/interests: " \
-    '\npython3 app.py --meeting ["frequency", "count"]' \
-    '\npython3 app.py --sellArrests lowerBoundCount upperBoundCount' \
-    '\n' \
-    '\nTo reach the drug sale arrests page, take the path /arrests/lower/upper', response.data)
+        self.assertEqual(b'Hello! Welcome to our website with the amazingly' \
+    b'curated title: Analyzing Criminal Drug Abuse Treatment in Females' \
+    b'\nAlso known as drug_abuse_treatment.py' \
+    b'\n' \
+    b'\n' \
+    b'Here are the main usages of our program for your research/interests: ' \
+    b'\npython3 app.py --meeting ["frequency", "count"]' \
+    b'\npython3 app.py --sellArrests lowerBoundCount upperBoundCount' \
+    b'\n' \
+    b'\nTo reach the drug sale arrests page, take the path /arrests/lower/upper', response.data)
 
 
 class TestDrugSaleArrests(unittest.TestCase):
@@ -32,13 +34,16 @@ class TestDrugSaleArrests(unittest.TestCase):
 
     def setUp(self):
         """Sets up the test client"""
+
         self.app = app.test_client()
 
-    """Tests the route for drug sale arrests"""
-    def test_route(self):
-        self.app = app.test_client()
+    
+    def test_drug_sale(self):
+        """Test for route for drug sale arrests"""
+
         response = self.app.get('/arrests/1/10', follow_redirects=True)
         self.assertEqual(b"283 people", response.data)
+
 
 if __name__ == "__main__":
     unittest.main()
--- app.py
+++ app.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 def homepage():
     """Determines the route to the homepage"""
 
-    return "Hello! Welcome to our website with the amazingly" \
-        "curated title: Analyzing Criminal Drug Abuse Treatment in Females" \
-        "\nAlso known as drug_abuse_treatment.py" \
-        "\n" \
-        "\n" \
-        "Here are the main usages of our program for your research/interests: " \
+    return 'Hello! Welcome to our website with the amazingly' \
+        'curated title: Analyzing Criminal Drug Abuse Treatment in Females' \
+        '\nAlso known as drug_abuse_treatment.py' \
+        '\n' \
+        '\n' \
+        'Here are the main usages of our program for your research/interests: ' \
         '\npython3 app.py --meeting ["frequency", "count"]' \
         '\npython3 app.py --sellArrests lowerBoundCount upperBoundCount' \
         '\n' \
